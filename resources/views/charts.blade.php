a30dfda3685976d886e95535a6c4f00d1bedb620
srabonipro <sraboni391@gmail.com>
2021-12-28 16:43:46 +0600

    add laravel chart

--- resources/views/charts.blade.php
+++ resources/views/charts.blade.php
@@ -8,12 +8,22 @@
 @endsection
 
 @section('scripts')
-    <script src="https://unpkg.com/echarts/dist/echarts.min.js"></script>
-    <script src="https://unpkg.com/@chartisan/echarts/dist/chartisan_echarts.js"></script>
+    {{-- <script src="https://unpkg.com/echarts/dist/echarts.min.js"></script>
+    <script src="https://unpkg.com/@chartisan/echarts/dist/chartisan_echarts.js"></script> --}}
+
+    <!-- Charting library -->
+    <script src="https://unpkg.com/chart.js@^2.9.3/dist/Chart.min.js"></script>
+    <!-- Chartisan -->
+    <script src="https://unpkg.com/@chartisan/chartjs@^2.1.0/dist/chartisan_chartjs.umd.js"></script>
+
+
     <script>
       const chart = new Chartisan({
         el: '#chart',
         url: "@chart('sales_chart')",
+        hooks: new ChartisanHooks()
+            .colors()
+            .datasets(['line', 'bar'])
       });
     </script>
 @endsection
